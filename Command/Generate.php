977a3a8787fd793bc25ada76bf35c1f368c3fb86
Florent <florent.g@allopneus.com>
2013-04-11 10:44:24 +0200

    command

--- Command/Generate.php
+++ Command/Generate.php
@@ -7,13 +7,25 @@
 namespace Trismegiste\Phytostatic\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Generate generates the site
- *
- * @author florent
  */
 class Generate extends Command
 {
+
+    protected function configure()
+    {
+        $this
+                ->setName('phyto:gene')
+                ->setDescription('Make the job !');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        echo "nothing";
+    }
 
 }
--- console.php
+++ console.php
@@ -9,9 +9,9 @@ namespace Trismegiste\Phytostatic;
 
 require_once 'vendor/autoload.php';
 
-use Trismegiste\Phytostatic\Command;
+use Trismegiste\Phytostatic\Command\Generate;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
-$application->add(new Command\Generate());
+$application->add(new Generate());
 $application->run();
